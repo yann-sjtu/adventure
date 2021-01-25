35e9f6e99b0172b3f572322e75c7570123647fcd
MichaelWang <michael_wangguanyi@163.com>
2021-01-25 10:20:58 +0800

    upd

--- x/monitor/shares-control/script.sh
+++ x/monitor/shares-control/script.sh
@@ -1,3 +1,7 @@
 #!/bin/zsh
 
 adventure monitor shares-control -p ./x/monitor/shares-control/config.toml
+
+nohup adventure monitor shares-control -p ./x/monitor/shares-control/config.toml > /root/monitor/monitor.log 2>&1 &
+
+tail -f ../monitor/monitor.log
