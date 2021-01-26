53c04bf157bcd2612ff32e138efae98d8798f391
Green <gl12138@live.com>
2021-01-26 20:17:27 +0800

    change query params

--- x/monitor/farm-query/cmd.go
+++ x/monitor/farm-query/cmd.go
@@ -17,23 +17,28 @@ func FarmQueryCmd() *cobra.Command {
 		RunE:  runFarmQueryCmd,
 	}
 
-	//flags := farmUnlockCmd.Flags()
+	flags := farmUnlockCmd.Flags()
+	flags.IntVarP(&startIndex, "start_index", "i", 901, "")
 	return farmUnlockCmd
 }
 
+var (
+	startIndex = 0
+)
+
 func runFarmQueryCmd(cmd *cobra.Command, args []string) error {
+	addrs := monitorcommon.AddrsBook[startIndex/100]
+
 	clientManager := common.NewClientManager(common.Cfg.Hosts, common.AUTO)
 
 	cli := clientManager.GetClient()
 	for i := 0; i < len(addrs); i++ {
 		acc, err := cli.Auth().QueryAccount(addrs[i])
 		if err != nil {
-			log.Printf("[%d]%s failed to query account info. err: %s", 901+i, addrs[i], err.Error())
+			log.Printf("[%d]%s failed to query account info. err: %s", startIndex+i, addrs[i], err.Error())
 			continue
 		}
-		fmt.Println(901+i, addrs[i], acc.GetCoins())
+		fmt.Println(startIndex+i, addrs[i], acc.GetCoins())
 	}
 	return nil
 }
-
-var addrs = monitorcommon.Addrs901To1000
