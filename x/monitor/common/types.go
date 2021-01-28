5ce1dc7ceabd4a63d6fe4a21e58a59d8b8872fe4
MichaelWang <michael_wangguanyi@163.com>
2021-01-28 16:42:17 +0800

    upd

---
+++ x/monitor/common/types.go
@@ -0,0 +1,30 @@
+package common
+
+import (
+	"fmt"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+type Worker struct {
+	accAddr sdk.AccAddress
+	index   int
+}
+
+func NewWorker(accAddr sdk.AccAddress, index int) Worker {
+	return Worker{
+		accAddr,
+		index,
+	}
+}
+
+func (w Worker) String() string {
+	return fmt.Sprintf("%s with index %d", w.accAddr.String(), w.index)
+}
+
+func (w *Worker) GetAccAddr() sdk.AccAddress {
+	return w.accAddr
+}
+
+func (w *Worker) GetIndex() int {
+	return w.index
+}
