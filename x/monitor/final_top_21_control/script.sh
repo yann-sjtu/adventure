27a09444a7c8b4100119f8806e0355ae1cf01822
MichaelWang <michael_wangguanyi@163.com>
2021-02-01 11:29:00 +0800

    upd

--- x/monitor/final_top_21_control/script.sh
+++ x/monitor/final_top_21_control/script.sh
@@ -1,7 +1,9 @@
 #!/bin/zsh
 
 adventure monitor final-top21-shares-control -p ./x/monitor/final_top_21_control/config.toml
-#
+# local
+nohup adventure monitor final-top21-shares-control -p ./x/monitor/final_top_21_control/config.toml > ~/log/monitor.log 2>&1 &
+
 #nohup adventure monitor final-top21-shares-control -p ./x/monitor/final_top_21_control/config.toml > /root/monitor/monitor.log 2>&1 &
 #
 #tail -f ../monitor/monitor.log
