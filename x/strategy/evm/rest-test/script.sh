92a90982e97dd1ecbcae121e7d500f9bdc5725a4
MichaelWang <michael_wangguanyi@163.com>
2021-02-24 21:49:56 +0800

    upd

--- x/strategy/evm/rest-test/script.sh
+++ x/strategy/evm/rest-test/script.sh
@@ -1,3 +1,5 @@
-adventure evm rest-test deploy -u 10.0.240.22:26659
+adventure evm rest-test deploy -u http://10.0.240.22:26659
 # get the contract address
-adventure evm rest-test run-erc20-transfer-test -c 0x0d021d10ab9E155Fc1e8705d12b73f9bd3de0a36 -u 10.0.240.22:26659
+adventure evm rest-test run-erc20-transfer-test -c 0xb4eb8fc3ab329c96a182ccd27671a41d46ba3a68 -u http://10.0.240.22:26659
+
+nohup adventure evm rest-test run-erc20-transfer-test -c 0xb4eb8fc3ab329c96a182ccd27671a41d46ba3a68 -u 10.0.240.22:26659 ~/log/rest_test.log 2>&1 &
