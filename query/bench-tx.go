54b0920823a9cc276c2d45e0990a63333b61eb02
Green <gl12138@live.com>
2021-08-03 18:19:54 +0800

    optimize

--- query/bench-tx.go
+++ query/bench-tx.go
@@ -4,6 +4,12 @@ import (
 	bytes2 "bytes"
 	"context"
 	"crypto/ecdsa"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/big"
+	"time"
+
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -11,11 +17,6 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 	adcomm "github.com/okex/adventure/common"
 	"github.com/spf13/cobra"
-	"io/ioutil"
-	"log"
-	"math/big"
-	"sync"
-	"time"
 )
 
 var (
@@ -52,93 +53,62 @@ func BenchTxCmd() *cobra.Command {
 	return cmd
 }
 
-var nonceMap = make(map[common.Address]uint64)
-
 func benchTx(cmd *cobra.Command, args []string) {
 	abiByte, err := ioutil.ReadFile(abiPath)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	sampleContractABI, err = abi.JSON(bytes2.NewReader(abiByte))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// 0.1 nonce
 	privkeys := adcomm.GetPrivKeyFromPrivKeyFile(privkPath)
-	for i := 0; i < concurrencyTx; i++ {
-		privateKey, _ := crypto.HexToECDSA(privkeys[i])
-		pubkeyECDSA, _ := privateKey.Public().(*ecdsa.PublicKey)
-		fromAddress := crypto.PubkeyToAddress(*pubkeyECDSA)
-		client, err := ethclient.Dial(host)
-		if err != nil {
-			log.Fatalf("failed to initialize client: %+v", err)
-		}
-		nonce, _ := client.PendingNonceAt(context.Background(), fromAddress)
-		nonceMap[fromAddress] = nonce
-		client.Close()
-	}
-
-	chainID := big.NewInt(int64(chainIdTx))
-	if err != nil {
-		log.Fatalf("failed to fetch the chain-id from network: %+v", err)
-	}
-
 	for {
 		for i := 0; i < concurrencyTx; i++ {
 			go func(privkey string) {
 				privateKey, _ := crypto.HexToECDSA(privkey)
-				pubkey := privateKey.Public()
-				pubkeyECDSA, ok := pubkey.(*ecdsa.PublicKey)
-				if !ok {
-					log.Fatalln("failed to switch secp256k1 private key -> pubkey")
-				}
-				fromAddress := crypto.PubkeyToAddress(*pubkeyECDSA)
-				// 0.5 get the gasPrice
-				gasPrice := big.NewInt(1000000000)
-				contractAddr := common.HexToAddress(contractAddress)
 				client, err := ethclient.Dial(host)
 				if err != nil {
 					log.Fatalf("failed to initialize client: %+v", err)
 				}
-				writeContract(client, fromAddress, gasPrice, chainID, privateKey, contractAddr)
+				nonce, _ := client.PendingNonceAt(context.Background(), getAddress(privateKey))
+
+				for {
+					unsignedTx := buildUnsignedTx(nonce, common.HexToAddress(contractAddress))
+					// 2. sign unsignedTx -> rawTx
+					signedTx, err := types.SignTx(unsignedTx, types.NewEIP155Signer(big.NewInt(int64(chainIdTx))), privateKey)
+					if err != nil {
+						log.Fatalf("failed to sign the unsignedTx offline: %+v", err)
+					}
+
+					// 3. send rawTx
+					err = client.SendTransaction(context.Background(), signedTx)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					nonce++
+					time.Sleep(time.Second * time.Duration(sleepTimeTx))
+				}
 			}(privkeys[i])
 		}
-		time.Sleep(time.Second * time.Duration(sleepTimeTx))
 	}
 
 }
 
-var mutex sync.Mutex
-
-func writeContract(client *ethclient.Client,
-	fromAddress common.Address,
-	gasPrice *big.Int,
-	chainID *big.Int,
-	privateKey *ecdsa.PrivateKey,
-	contractAddr common.Address) {
-	// 0. get the value of nonce, based on address
-	mutex.Lock()
-	nonce := nonceMap[fromAddress]
-	nonceMap[fromAddress] = nonce + 1
-	mutex.Unlock()
-
-	unsignedTx := writeContractTx(nonce, contractAddr, gasPrice)
-	// 2. sign unsignedTx -> rawTx
-	signedTx, err := types.SignTx(unsignedTx, types.NewEIP155Signer(chainID), privateKey)
-	if err != nil {
-		log.Fatalf("failed to sign the unsignedTx offline: %+v", err)
+func getAddress(privateKey *ecdsa.PrivateKey) common.Address  {
+	pubkeyECDSA, ok := privateKey.Public().(*ecdsa.PublicKey)
+	if ok != true {
+		panic(fmt.Errorf("convert into pubkey failed"))
 	}
-
-	// 3. send rawTx
-	err = client.SendTransaction(context.Background(), signedTx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	client.Close()
+	fromAddress := crypto.PubkeyToAddress(*pubkeyECDSA)
+	return fromAddress
 }
 
-func writeContractTx(nonce uint64, contractAddr common.Address, gasPrice *big.Int) *types.Transaction {
+func buildUnsignedTx(nonce uint64, contractAddr common.Address) *types.Transaction {
+	gasPrice := big.NewInt(1000000000)
 	value := big.NewInt(0)
 	gasLimit := uint64(3000000)
 
