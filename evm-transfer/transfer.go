6a43d07607db9e2ad0906b614e5f023939393256
Green <>
2021-10-11 13:01:59 +0800

    change rest -> rpc

--- evm-transfer/transfer.go
+++ evm-transfer/transfer.go
@@ -1,31 +1,35 @@
 package evm_transfer
 
 import (
-	"context"
 	"crypto/ecdsa"
 	"fmt"
 	"log"
-	"math/big"
-	"sync"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	ethcmm "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/ethclient"
-	adcomm "github.com/okex/adventure/common"
-	"github.com/okex/exchain-ethereum-compatible/utils"
+	"github.com/okex/adventure/common"
+	gosdk "github.com/okex/exchain-go-sdk"
+	"github.com/okex/exchain-go-sdk/types"
+	"github.com/okex/exchain-go-sdk/utils"
 	"github.com/spf13/cobra"
 	"github.com/tendermint/tendermint/libs/rand"
+)
+
+var (
+	to = ethcmm.BytesToAddress(crypto.Keccak256(rand.Bytes(64)))
 )
 
 var (
 	concurrencyTx   int
 	sleepTimeTx     int
 	privkPath       string
-	rest_hosts    []string
+	rpc_hosts    []string
 
-	chainID       *big.Int
+	chainID       string
+
+	fixed  bool
 )
 
 func TransferCmd() *cobra.Command {
@@ -39,90 +43,68 @@ func TransferCmd() *cobra.Command {
 	flags.IntVar(&concurrencyTx, "concurrency", 10, "set the number of tx number per second")
 	flags.IntVar(&sleepTimeTx, "sleepTime", 1, "")
 
-	flags.StringVar(&privkPath, "privkeyPath", "", "")
-	flags.StringSliceVar(&rest_hosts, "rest-hosts", []string{}, "")
+	flags.StringVar(&privkPath, "privkey-path", "", "")
+	flags.StringSliceVar(&rpc_hosts, "rpc-hosts", []string{}, "")
+	flags.StringVar(&chainID, "chain-id", "", "")
+
+	flags.BoolVar(&fixed, "fixed", false, "")
 	return cmd
 }
 
 func transferTx(cmd *cobra.Command, args []string) {
-	chainID = getChainId()
-	log.Println("chain id", chainID.Uint64())
-
-	privkeys := adcomm.GetPrivKeyFromPrivKeyFile(privkPath)
-	var wg sync.WaitGroup
-	wg.Add(concurrencyTx)
+	privkeys := common.GetPrivKeyFromPrivKeyFile(privkPath)
 	for i := 0; i < concurrencyTx; i++ {
 		go func(index int, privkey string) {
-			defer wg.Done()
-
-			restHost := rest_hosts[index%len(rest_hosts)]
-			transfer(privkey, restHost)
+			rpcHost := rpc_hosts[index%len(rpc_hosts)]
+			transfer(privkey, rpcHost)
 
 		}(i, privkeys[i])
 	}
-	wg.Wait()
-}
 
-func getChainId() *big.Int {
-	client, err := ethclient.Dial(rest_hosts[0])
-	if err != nil {
-		panic(err)
-	}
-
-	id, err := client.ChainID(context.Background())
-	if err != nil {
-		panic(err)
-	}
-	return id
+	select {}
 }
 
 func transfer(privkey string, host string) {
-	privateKey, _ := crypto.HexToECDSA(privkey)
-	client, err := ethclient.Dial(host)
-	if err != nil {
-		log.Fatalf("failed to initialize client: %+v", err)
-	}
+	cfg, _ := types.NewClientConfig(host, chainID, types.BroadcastSync, "", 30000000, 1.5, "0.0000000001"+common.NativeToken)
+	cli := gosdk.NewClient(cfg)
 
-	nonce, err := client.PendingNonceAt(context.Background(), getEthAddress(privateKey))
+	addr := getCosmosAddress(privkey)
+	accInfo, err := cli.Auth().QueryAccount(addr.String())
 	if err != nil {
-		log.Fatalf("failed to fetch noce: %+v", err)
+		panic(err)
 	}
+	nonce := accInfo.GetSequence()
 
 	for {
-		tx := types.NewTransaction(
-			nonce,
-			common.BytesToAddress(crypto.Keccak256(rand.Bytes(64))),
-			big.NewInt(1),
-			uint64(21000),
-			big.NewInt(1e9),
-			nil)
-		signedTx, err := types.SignTx(
-			tx,
-			types.NewEIP155Signer(chainID),
-			privateKey,
-		)
+		if !fixed {
+			to = ethcmm.BytesToAddress(crypto.Keccak256(rand.Bytes(64)))
+		}
+		res, err := cli.Evm().SendTxEthereum(privkey, to.String(), "0.000000001", "",21000, nonce)
 		if err != nil {
-			log.Fatalf("failed to sign the unsignedTx offline: %+v", err)
+			continue
+		} else {
+			log.Printf("txhash: %s\n", res.TxHash)
 		}
 
-		// 3. send rawTx
-		err = client.SendTransaction(context.Background(), signedTx)
-		if err != nil {
-			log.Printf("[%s] err: %s\n", getEthAddress(privateKey), err)
-			continue
-		}
-		txhash, _ := utils.Hash(signedTx)
-		fmt.Println(txhash)
 		nonce++
 		time.Sleep(time.Second * time.Duration(sleepTimeTx))
 	}
 }
 
-func getEthAddress(privateKey *ecdsa.PrivateKey) common.Address {
+func getEthAddress(privateKey *ecdsa.PrivateKey) ethcmm.Address {
 	pubkeyECDSA, ok := privateKey.Public().(*ecdsa.PublicKey)
 	if ok != true {
 		panic(fmt.Errorf("convert into pubkey failed"))
 	}
 	fromAddress := crypto.PubkeyToAddress(*pubkeyECDSA)
 	return fromAddress
+}
+
+func getCosmosAddress(privkey string) sdk.Address {
+	privateKey, _ := crypto.HexToECDSA(privkey)
+	cosmosAddr, err := utils.ToCosmosAddress(getEthAddress(privateKey).String())
+	if err != nil {
+		panic(err)
+	}
+	return cosmosAddr
 }
