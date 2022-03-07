1b156293c7c38d0d4541bf57bb34279430795fbe
lianghengbao <hengbao.liang@okg.com>
2022-03-07 12:51:32 +0800

    correct output comments

--- evm/bench/utils/tx.go
+++ evm/bench/utils/tx.go
@@ -106,8 +106,8 @@ func RunTxRpc(p BasepParam, e func(ethcmm.Address) []TxParam) {
 	duration = time.Since(startTime).Milliseconds()
 	elapsed := strconv.FormatInt(time.Since(startTime).Milliseconds(), 10) + "ms"
 	ratio, tps = GetTxTpsAndSuccessRatio(lstTxHash,int64(p.concurrency))
-	log.Printf(" %d tx send and receive txhash and cost time: %s\n", p.concurrency, elapsed)
-	log.Printf(" tx send success ratio is : %d, and tx tps is : %d\n", ratio, tps)
+	log.Printf(" %d tx sent and received txhash and total time cost: %s\n", p.concurrency, elapsed)
+	log.Printf(" %d tx send success, and sucess ratio is : %d, and tx tps is : %d\n", len(lstTxHash), ratio, tps)
 }
 
 func RunTxs(p BasepParam, e func(ethcmm.Address) []TxParam) {
