fc92c9ac729eb553ad14339336dabd3ddd9d7ab5
lianghengbao <hengbao.liang@okg.com>
2022-03-07 17:58:27 +0800

    add more p

--- evm/bench/transfer/txrpc.go
+++ evm/bench/transfer/txrpc.go
@@ -8,9 +8,11 @@ import (
 	sdk "github.com/okex/exchain/libs/cosmos-sdk/types"
 	"github.com/okex/exchain/libs/tendermint/libs/rand"
 	"github.com/spf13/cobra"
+	"runtime"
 )
 
 func txrpc(cmd *cobra.Command, args []string) {
+	runtime.GOMAXPROCS(64)
 	amount := sdk.MustNewDecFromStr("0.00001").Int
 	fixedAddr := ethcmm.BytesToAddress(crypto.Keccak256(rand.Bytes(64)))
 	var toAddrs []ethcmm.Address
